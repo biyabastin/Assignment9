3738b025341d5f69b7bb6729d342cac646241d4c
biyabastin <connectbiya@gmail.com>
2026-02-24 00:36:17 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,10 +1,16 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, OrderViewSet, OrderItemViewSet
+from .views import add_to_cart, view_cart
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
 router.register(r'orders', OrderViewSet)
 router.register(r'order-items', OrderItemViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('cart/add/<int:product_id>/', add_to_cart),
+    path('cart/', view_cart),
+]
 
+urlpatterns += router.urls
--- views.py
+++ views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import viewsets
+from .models import Product, Order, OrderItem
+from .serializers import ProductSerializer, OrderSerializer, OrderItemSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+
+def add_to_cart(request, product_id):
+    cart = request.session.get('cart', {})
+
+    if product_id in cart:
+        cart[product_id] += 1
+    else:
+        cart[product_id] = 1
+
+    request.session['cart'] = cart
+
+    return JsonResponse({"message": "Product added to cart", "cart": cart})
+
+def view_cart(request):
+    cart = request.session.get('cart', {})
+    return JsonResponse({"cart": cart})
