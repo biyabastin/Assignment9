d5eb46c1108c1225fcbd650adedca59cc587b947
biyabastin <connectbiya@gmail.com>
2026-02-24 00:18:11 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, OrderViewSet, OrderItemViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'orders', OrderViewSet)
+router.register(r'order-items', OrderItemViewSet)
+
+urlpatterns = router.urls
+
